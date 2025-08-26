b3fd23ae3ee51946794095b6597695013d799e93
Marta <marta.kolodzinska@cognizant.com>
2025-08-26 10:03:34 +0200

    Ex. 2

--- app/src/main/java/com/example/introtocompose/MainActivity.kt
+++ app/src/main/java/com/example/introtocompose/MainActivity.kt
@@ -4,15 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.introtocompose.ui.theme.IntroToComposeTheme
@@ -22,52 +30,44 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-//            IntroToComposeTheme {
-//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-//                    Greeting(
-//                        name = "Android",
-//                        modifier = Modifier.padding(innerPadding)
-//                    )
-//                }
-//            }
             IntroToComposeTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize().padding(all = 59.dp),
-                    color = MaterialTheme.colorScheme.primary
-                ) {
-                    Greeting("Android")
-                }
+
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        color = Color(0xFF546E7A)
+    ) {
+        Text("Hello")
+    }
 }
 
+@Preview
 @Composable
-fun Greetings(name: String) {
-    Text(text = "Hello $name")
-}
+fun CreateCircle() {
+    Card(modifier = Modifier
+        .padding(3.dp)
+        .size(45.dp),
+        shape = CircleShape
+    )  {
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Text(text = "Tap")
+        }
 
-@Composable
-fun ShowAge(age: Int = 12) {
-    Text(text = age.toString())
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     IntroToComposeTheme {
-        Column {
-            Greeting("Android")
-            ShowAge(age = 34)
-        }
-
+        MyApp()
     }
 }
