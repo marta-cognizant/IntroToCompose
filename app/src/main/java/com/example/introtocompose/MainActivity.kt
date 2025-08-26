76887b0bbb675196d1a15d5aca88888b7ea39df0
Marta <marta.kolodzinska@cognizant.com>
2025-08-26 11:21:39 +0200

    Ex. 3

--- app/src/main/java/com/example/introtocompose/MainActivity.kt
+++ app/src/main/java/com/example/introtocompose/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.introtocompose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,6 +28,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.introtocompose.ui.theme.IntroToComposeTheme
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +43,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             IntroToComposeTheme {
-
+                MyApp()
             }
         }
     }
@@ -42,23 +54,40 @@ fun MyApp() {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
-            .fillMaxHeight(),
-        color = Color(0xFF546E7A)
+            .fillMaxHeight(), color = Color(0xFF546E7A)
     ) {
-        Text("Hello")
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "$100", style = TextStyle(
+                    color = Color.White, fontSize = 39.sp, fontWeight = FontWeight.ExtraBold
+                )
+            )
+            Spacer(modifier = Modifier.height(30.dp))
+            CreateCircle()
+        }
     }
 }
 
 @Preview
 @Composable
 fun CreateCircle() {
-    Card(modifier = Modifier
-        .padding(3.dp)
-        .size(45.dp),
-        shape = CircleShape
-    )  {
+    var moneyCounter by remember {
+        mutableStateOf(0)
+    }
+    Card(
+        modifier = Modifier
+            .padding(3.dp)
+            .size(105.dp)
+            .clickable {
+                moneyCounter += 1
+                Log.d("Tap", "CreateCircle: Tap: $moneyCounter")
+            }, shape = CircleShape, elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Text(text = "Tap")
+            Text(text = "Tap $moneyCounter", modifier = Modifier)
         }
 
     }
