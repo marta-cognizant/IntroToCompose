efe9d7d95831918d1465873af1d0fdfa9b5969ad
Marta <marta.kolodzinska@cognizant.com>
2025-09-09 13:50:03 +0200

    Movies app sketch

--- app/src/main/java/com/example/introtocompose/MainActivity.kt
+++ app/src/main/java/com/example/introtocompose/MainActivity.kt
@@ -1,108 +1,130 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.introtocompose
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.introtocompose.ui.theme.IntroToComposeTheme
-import androidx.compose.material3.CardDefaults
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            IntroToComposeTheme {
-                MyApp()
+            MyApp {
+                MainContent()
             }
         }
     }
 }
 
 @Composable
-fun MyApp() {
-    var moneyCounter = remember {
-        mutableStateOf(0)
-    }
-    Surface(
-        modifier = Modifier
-            .fillMaxWidth()
-            .fillMaxHeight(), color = Color(0xFF546E7A)
-    ) {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "$${moneyCounter.value}", style = TextStyle(
-                    color = Color.White, fontSize = 39.sp, fontWeight = FontWeight.ExtraBold
+fun MyApp(content: @Composable () -> Unit) {
+    IntroToComposeTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text("Movies")
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = Color.Magenta
+                    ),
                 )
-            )
-            Spacer(modifier = Modifier.height(30.dp))
-            CreateCircle(moneyCounter = moneyCounter.value) {
-                newValue -> moneyCounter.value = newValue
             }
-            if (moneyCounter.value > 25) {
-                Spacer(modifier = Modifier.height(30.dp))
-                Text(text = "Lot's of money!")
+        ) { paddingValues ->
+            Surface(modifier = Modifier.padding(top = paddingValues.calculateTopPadding())) {
+                content()
             }
         }
     }
 }
 
-@Preview
 @Composable
-fun CreateCircle(moneyCounter: Int = 0, updatedMoneyCounter: (Int) -> Unit) {
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Avatar",
+        "300",
+        "Harry Potter",
+        "Little Mermaid",
+        "Life of Pi",
+        "Mission Impossible"
+    )
+) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn {
+            items(items = movieList) { item ->
+                MovieRow(item)
+            }
+        }
 
+    }
+}
+
+@Composable
+fun MovieRow(movie: String) {
     Card(
         modifier = Modifier
-            .padding(3.dp)
-            .size(105.dp)
-            .clickable {
-                updatedMoneyCounter(moneyCounter + 1)
-                Log.d("Tap", "CreateCircle: Tap: $moneyCounter")
-            }, shape = CircleShape, elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp)
     ) {
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Text(text = "Tap ${moneyCounter}", modifier = Modifier)
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                tonalElevation = 4.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+            }
+            Text(text = movie)
         }
+
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    IntroToComposeTheme {
-        MyApp()
+fun DefaultPreview() {
+    MyApp {
+        MainContent()
     }
 }
