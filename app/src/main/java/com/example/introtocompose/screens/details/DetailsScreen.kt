2b904e301c2629bd656d3c51a8fb784d7568a14e
Marta <marta.kolodzinska@cognizant.com>
2025-09-10 09:26:37 +0200

    Navigation - improvement.

--- app/src/main/java/com/example/introtocompose/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/introtocompose/screens/details/DetailsScreen.kt
@@ -1,27 +1,76 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.introtocompose.screens.details
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun DetailsScreen(navController: NavController, movieData: String?) {
-    Surface(modifier = Modifier
-        .fillMaxHeight()
-        .fillMaxWidth()) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {
+                Text("Movies")
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color.LightGray
+            ),
+            navigationIcon = {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Back",
+                    modifier = Modifier
+                        .clickable {
+                            navController.popBackStack()
+                        }
+                        .padding(horizontal = 12.dp)
+                )
+            }
+        )
+    }) { innerPadding ->
+        Surface(
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .padding(innerPadding)
         ) {
-            Text(text = movieData.toString(), style = MaterialTheme.typography.headlineMedium)
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = movieData.toString(), style = MaterialTheme.typography.headlineMedium)
+                Spacer(modifier = Modifier.height(23.dp))
+//                Button(onClick = { navController.popBackStack() }) {
+//                    Text(text = "Go Back")
+//                }
+            }
         }
     }
+
+
 }
