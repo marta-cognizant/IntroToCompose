fee54b2efd530ac2b283525a40b40145f1e218d2
Marta <marta.kolodzinska@cognizant.com>
2025-09-10 11:08:50 +0200

    Updated movies.

---
+++ app/src/main/java/com/example/introtocompose/widgets/Widgets.kt
@@ -0,0 +1,69 @@
+package com.example.introtocompose.widgets
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.example.introtocompose.model.Movie
+import com.example.introtocompose.model.getMovies
+
+@Preview(showBackground = true)
+@Composable
+fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp)
+            .clickable {
+                onItemClick(movie.id)
+            },
+        shape = RoundedCornerShape(corner = CornerSize(16.dp)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 6.dp),
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                tonalElevation = 4.dp
+            ) {
+                AsyncImage(
+                    model = movie.poster,
+                    contentDescription = "'${movie.title}' poster",
+                )
+            }
+            Column(modifier = Modifier
+                .padding(4.dp)
+                .fillMaxWidth()) {
+                Text(text = movie.title, style = MaterialTheme.typography.headlineMedium)
+                Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.bodyLarge)
+                Text(text = "Released: ${movie.year}", style = MaterialTheme.typography.bodyLarge)
+            }
+        }
+    }
+}
