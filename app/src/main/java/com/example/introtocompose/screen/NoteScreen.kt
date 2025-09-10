9406ea07628e1c526e8baccf39c2bd64238dec66
Marta <marta.kolodzinska@cognizant.com>
2025-09-10 14:29:28 +0200

    Added core of Note App main screen.

--- app/src/main/java/com/example/introtocompose/screen/NoteScreen.kt
+++ app/src/main/java/com/example/introtocompose/screen/NoteScreen.kt
@@ -4,6 +4,7 @@ package com.example.introtocompose.screen
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Create
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -12,6 +13,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,33 +24,60 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.introtocompose.R
+import com.example.introtocompose.components.NoteButton
 import com.example.introtocompose.components.NoteInputText
 
 @Composable
 fun NoteScreen() {
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+
     Column() {
         TopAppBar(
-            title = { Text(
-                text = stringResource(id = R.string.app_name),
-                fontStyle = FontStyle.Normal,
-                fontWeight = FontWeight.Bold,
-                color = Color.Black
-            )
-             },
+            title = {
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    fontStyle = FontStyle.Normal,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+            },
             actions = {
                 Icon(Icons.Rounded.Create, contentDescription = "App Icon")
             },
             colors = TopAppBarDefaults.topAppBarColors(titleContentColor = Color(0xFFDADFE3))
         )
         // Content
-        Column(modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             NoteInputText(
-                text = "Hello",
-                label = "Hello",
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
+                text = title,
+                label = "Title",
                 onTextChange = {
+                    if (it.all { char ->
+                        char.isLetter() || char.isWhitespace() || char.isDigit()
+                        }) title = it
+                }
+            )
+            NoteInputText(
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
+                text = description,
+                label = "Add a note",
+                onTextChange = {
+                    if (it.all { char ->
+                            char.isLetter() || char.isWhitespace() || char.isDigit()
+                        }) description = it
+                }
+            )
 
+            NoteButton(
+                text = "Save",
+                onClick = {
                 }
             )
         }
