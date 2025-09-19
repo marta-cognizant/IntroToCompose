d42138d14b5dd6058141585992b9c3911b969fdc
Marta <marta.kolodzinska@cognizant.com>
2025-09-19 17:15:44 +0200

    Added top app bar.

--- app/src/main/java/com/example/introtocompose/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/introtocompose/screens/main/MainScreen.kt
@@ -1,30 +1,58 @@
 package com.example.introtocompose.screens.main
 
+import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.introtocompose.data.DataOrException
 import com.example.introtocompose.model.Weather
+import com.example.introtocompose.widgets.WeatherAppBar
 
 @Composable
 fun MainScreen(navController: NavController, mainViewModel: MainViewModel = hiltViewModel()) {
-    ShowData(mainViewModel)
+    val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
+        initialValue = DataOrException(loading = true)
+    ) {
+        value = mainViewModel.getWeatherData(city = "Seattle")
+    }.value
+
+    if (weatherData.loading == true) {
+        CircularProgressIndicator()
+    } else if (weatherData.data != null) {
+        MainScaffold(weather = weatherData.data!!, navController)
+    }
+}
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun MainScaffold(weather: Weather, navController: NavController) {
+    Scaffold(topBar = {
+        WeatherAppBar(
+            title = weather.city.name + ", ${weather.city.country}",
+            icon = Icons.Default.ArrowBack,
+            navController = navController,
+            elevation = 5.dp,
+        ) {
+            Log.d("WEATHER_APP_BAR", "ButtonClicked!")
+        }
+    }) { paddingValues ->
+        Scaffold(modifier = Modifier.padding(paddingValues)) {
+            MainContent(data = weather)
+        }
+    }
 }
 
 @Composable
-fun ShowData(mainViewModel: MainViewModel) {
-    val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
-        initialValue = DataOrException(loading = true)) {
-            value = mainViewModel.getWeatherData(city = "Seattle")
-        }.value
-
-    if(weatherData.loading == true) {
-        CircularProgressIndicator()
-    } else if (weatherData.data != null) {
-        Text("Main Screen: ${weatherData.data!!.toString()}")
-    }
-
+fun MainContent(data: Weather) {
+    Text(text = data.city.name)
 }
