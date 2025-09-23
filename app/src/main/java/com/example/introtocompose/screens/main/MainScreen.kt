659224cef8b6a2453ded46fc14391b7bb224ca05
Marta <marta.kolodzinska@cognizant.com>
2025-09-23 13:27:22 +0200

    Added top circle widget.

--- app/src/main/java/com/example/introtocompose/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/introtocompose/screens/main/MainScreen.kt
@@ -2,20 +2,36 @@ package com.example.introtocompose.screens.main
 
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil3.compose.AsyncImage
+import com.example.introtocompose.R
 import com.example.introtocompose.data.DataOrException
 import com.example.introtocompose.model.Weather
+import com.example.introtocompose.utils.formatDate
+import com.example.introtocompose.utils.formatDecimals
 import com.example.introtocompose.widgets.WeatherAppBar
 
 @Composable
@@ -23,7 +39,7 @@ fun MainScreen(navController: NavController, mainViewModel: MainViewModel = hilt
     val weatherData = produceState<DataOrException<Weather, Boolean, Exception>>(
         initialValue = DataOrException(loading = true)
     ) {
-        value = mainViewModel.getWeatherData(city = "Seattle")
+        value = mainViewModel.getWeatherData(city = "Paris")
     }.value
 
     if (weatherData.loading == true) {
@@ -54,5 +70,69 @@ fun MainScaffold(weather: Weather, navController: NavController) {
 
 @Composable
 fun MainContent(data: Weather) {
-    Text(text = data.city.name)
+    val imageUrl = "https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png"
+    val temperature = formatDecimals(data.list[0].temp.day) + "°"
+    val description = data.list[0].weather[0].main
+    Log.d("WEATHER_APP", "Image URL: $imageUrl")
+
+    Column(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = formatDate(data.list[0].dt),
+            style = MaterialTheme.typography.bodyMedium,
+            color = Color.Black,
+            // color = MaterialTheme.colorScheme.onSecondary,
+            fontWeight = FontWeight.SemiBold,
+            modifier = Modifier.padding(6.dp)
+        )
+
+        Surface(
+            modifier = Modifier
+                .padding(4.dp)
+                .size(200.dp),
+            shape = CircleShape,
+            color = Color(0xFFFFC400)
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                WeatherStateImage(imageUrl = imageUrl)
+                Text(
+                    text = temperature, style = MaterialTheme.typography.headlineLarge,
+                    fontWeight = FontWeight.ExtraBold
+                )
+                Text(text = description, fontStyle = FontStyle.Italic)
+            }
+        }
+    }
+}
+
+@Composable
+fun WeatherStateImage(imageUrl: String) {
+
+    AsyncImage(
+        model = imageUrl, contentDescription = "icon image",
+        modifier = Modifier.size(80.dp),
+        onLoading = {
+            Log.d("ASYNC_IMAGE", "Loading...")
+        },
+        onSuccess = {
+            Log.d("ASYNC_IMAGE", "Success...")
+        },
+        onError = {
+            Log.d("ASYNC_IMAGE", "Error... ${it.result.throwable}")
+            Log.d("ASYNC_IMAGE", "${it.result.throwable.message}")
+            Log.d("ASYNC_IMAGE", "${it.result.throwable.cause}")
+        },
+        placeholder = painterResource(R.drawable.sun),
+        error = painterResource(R.drawable.sun)
+
+    )
+
 }
--- app/src/main/java/com/example/introtocompose/screens/main/MainViewModel.kt
+++ app/src/main/java/com/example/introtocompose/screens/main/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import coil3.ImageLoader
 import com.example.introtocompose.data.DataOrException
 import com.example.introtocompose.model.Weather
 import com.example.introtocompose.repository.WeatherRepository
@@ -14,7 +15,9 @@ import jakarta.inject.Inject
 import kotlinx.coroutines.launch
 
 @HiltViewModel
-class MainViewModel @Inject constructor(private val repository: WeatherRepository) : ViewModel() {
+class MainViewModel @Inject constructor(
+    private val repository: WeatherRepository,
+) : ViewModel() {
     suspend fun getWeatherData(city: String): DataOrException<Weather, Boolean, Exception> {
         return repository.getWeather(cityQuery = city)
     }
