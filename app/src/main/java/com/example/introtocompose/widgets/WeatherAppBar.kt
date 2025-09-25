f849e9d1146a2ef82ddb815ac3048a3428acc32b
Marta <marta.kolodzinska@cognizant.com>
2025-09-25 09:56:58 +0200

    Added logic to 'Favorite' button, and toast.

--- app/src/main/java/com/example/introtocompose/widgets/WeatherAppBar.kt
+++ app/src/main/java/com/example/introtocompose/widgets/WeatherAppBar.kt
@@ -1,5 +1,7 @@
 package com.example.introtocompose.widgets
 
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -25,6 +27,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -63,6 +66,11 @@ fun WeatherAppBar(
     val showDialog = rememberSaveable {
         mutableStateOf(false)
     }
+    val showIt = rememberSaveable {
+        mutableStateOf(false)
+    }
+    val context = navController.context
+
     if (showDialog.value) {
         ShowSettingsDropDownMenu(showDialog = showDialog, navController = navController)
     }
@@ -118,28 +126,46 @@ fun WeatherAppBar(
                     })
             }
             if (isMainScreen) {
-                Icon(
-                    imageVector = Icons.Default.Favorite,
-                    contentDescription = "favorites icon",
-                    modifier = Modifier
-                        .scale(0.9f)
-                        .clickable {
-                            val dataList = title.split(",")
-                            favoriteViewMode.insertFavorite(
-                                Favorite(
-                                    city = dataList[0],
-                                    country = dataList[1].trim()
-                                )
-                            )
-                        },
-                    tint = Color.Red.copy(alpha = 0.6f)
-                )
+                val isAlreadyFavList = favoriteViewMode.favList.collectAsState().value.filter { item ->
+                    (item.city == title.split(",")[0])
+                }
+                if (isAlreadyFavList.isNullOrEmpty()) {
+                    Icon(
+                        imageVector = Icons.Default.Favorite,
+                        contentDescription = "favorites icon",
+                        modifier = Modifier
+                            .scale(0.9f)
+                            .clickable {
+                                val dataList = title.split(",")
+                                favoriteViewMode.insertFavorite(
+                                    Favorite(
+                                        city = dataList[0],
+                                        country = dataList[1].trim()
+                                    )
+                                ).run {
+                                    showIt.value = true
+                                }
+                            },
+                        tint = Color.Red.copy(alpha = 0.6f)
+                    )
+                } else {
+                    showIt.value = false
+                    Box{}
+                }
+                ShowToast(context = context, showIt = showIt)
             }
         },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = Color.Transparent
         ),
     )
+}
+
+@Composable
+fun ShowToast(context: Context, showIt: MutableState<Boolean>) {
+    if (showIt.value) {
+        Toast.makeText(context, "Added to Favorites", Toast.LENGTH_SHORT).show()
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
